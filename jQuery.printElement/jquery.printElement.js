45e9236f38adb86e217aa8b48000d325965bea5d
Gregory Waxman <akkuma+github@gmail.com>
2011-05-17 11:35:24 -0400

    Removed overrideElementCSS and replaced with the ability to opt out of importing stylesheets (importStylesheets), the ability to exclude specific stylesheets (stylesheetsToExclude), and the ability to add stylesheets (stylesheets).
    Call proper focus (per Mozilla documentation contentWindow.focus), switched from calling the printPage function to using onload to trigger the printPage, added in a html5 doctype, switched to using document.execCommand for printing where available.

--- jQuery.printElement/jquery.printElement.js
+++ jQuery.printElement/jquery.printElement.js
@@ -30,11 +30,13 @@
 			* 2 : array of $.fn.printElement.cssElement (s)
 			* 3 : array of strings with paths to alternate css files (optimized for print)
 			*/
-			overrideElementCSS: null,
+			importStylesheets: true,
 			printBodyOptions: {
 				styleToAdd: 'padding:10px;margin:10px;', //style attributes to add to the body of print document
 				classNameToAdd: '' //css class to add to the body of print document
 			},
+			stylesheets: null,
+			stylesheetsToExclude: null,
 			leaveOpen: false, // in case of popup, leave the print page open or not
 			iframeElementOptions: {
 				styleToAdd: 'border:none;position:absolute;width:0px;height:0px;bottom:0px;left:0px;', //style attributes to add to the iframe element
@@ -67,36 +69,22 @@
         else {
             //The random ID is to overcome a safari bug http://www.cjboco.com.sharedcopy.com/post.cfm/442dc92cd1c0ca10a5c35210b8166882.html
             var printElementID = "printElement_" + (Math.round(Math.random() * 99999)).toString(),
-				iframe = $('<iframe />', {
-					style: opts["iframeElementOptions"]["styleToAdd"],
-					id: printElementID,
-					className: opts["iframeElementOptions"]["classNameToAdd"],
-					frameBorder: 0,
-					scrolling: 'no',
-					src: 'about:blank'
-				}).appendTo(document.body)[0];
+				iframe = $('<iframe id="' + printElementID + '" '
+                                + 'src="about:blank" '
+                                + 'scrolling="no" '
+                                + 'frameBorder="0" '
+                                + 'class="' + opts["iframeElementOptions"]["classNameToAdd"] + '" '
+                                + 'style="' + opts["iframeElementOptions"]["styleToAdd"] + '" />').appendTo(document.body)[0];
 			
             documentToWriteTo = iframe.contentWindow || iframe.contentDocument;
             documentToWriteTo.document && (documentToWriteTo = documentToWriteTo.document);
             popupOrIframe = iframe.contentWindow || iframe;
-			iframe.focus();
         }
 
+        popupOrIframe.focus();
         documentToWriteTo.open();
         documentToWriteTo.write(html);
         documentToWriteTo.close();
-        _callPrint(popupOrIframe);
-    }
-    
-	function _callPrint(element) {
-        if (element && element.printPage) {
-            element.printPage();
-		}
-        else {
-            setTimeout(function () {
-                _callPrint(element);
-            }, 50);
-		}
     }
 	
     function _getBaseHref() {
@@ -108,35 +96,48 @@
         var elementHtml = $element.html(),
 			html = [];
 			
-        html.push('<html><head><title>' + opts.pageTitle + '</title>');
-        if (opts.overrideElementCSS) {
-            if (opts.overrideElementCSS.length > 0) {
-                for (var x = 0, l = opts.overrideElementCSS.length; x < l; x++) {
-                    var current = opts.overrideElementCSS[x];
-                    if (typeof (current) === 'string') {
-                        html.push('<link type="text/css" rel="stylesheet" href="' + current + '" >');
-                    }
-					else {
-                        html.push('<link type="text/css" rel="stylesheet" href="' + current["href"] + '" media="' + current["media"] + '" >');
-					}
-				}
-            }
-        }
-        else {
+        html.push('<!DOCTYPE html><html><head><title>' + opts.pageTitle + '</title>');
+		
+        if (opts.importStylesheets) {
             $("link", document).filter(function () {
-                return $(this).attr("rel").toLowerCase() == "stylesheet";
+				var include = false;
+				var isStylesheet = this.rel.toLowerCase() === 'stylesheet';
+				if (isStylesheet && opts.stylesheetsToExclude) {
+					var href = this.href;
+					$.each(opts.stylesheetsToExclude, function (key, val) {
+						if (href.indexOf(val) > -1) {
+							include = false;
+							return false;
+						}
+						else {
+							include = true;
+						}
+					});
+				}
+				else if (isStylesheet) {
+					include = true;
+				}
+				
+				return include;
             }).each(function () {
-				var $this = $(this);
-                html.push('<link type="text/css" rel="stylesheet" href="' + $this.attr("href") + '" media="' + $this.attr('media') + '" >');
+				var media = this.media ? 'media="'+ this.media +'"' : '';
+                html.push('<link type="text/css" rel="stylesheet" href="' + this.href + '" ' + media + ' >');
             });
         }
+		
+        if (opts.stylesheets) {
+		    $.each(opts.stylesheets, function(key, val) {
+			    html.push('<link type="text/css" rel="stylesheet" href="' + val + '" media="print" >');
+		    });
+        }
+
         //Ensure that relative links work
         html.push('<base href="' + _getBaseHref() + '" />');
-        html.push('</head><body style="' + opts.printBodyOptions.styleToAdd + '" class="' + opts.printBodyOptions.classNameToAdd + '">');
+        html.push('</head><body onload="printPage()" style="' + opts.printBodyOptions.styleToAdd + '" class="' + opts.printBodyOptions.classNameToAdd + '">');
         html.push('<div class="' + $element.attr('class') + '">' + elementHtml + '</div>');
-        html.push('<script type="text/javascript">function printPage(){focus();print();' + ((!opts["leaveOpen"] && opts["printMode"].toLowerCase() == 'popup') ? 'close();' : '') + '}</script>');
+        html.push('<script type="text/javascript">function printPage(){focus();try {document.execCommand("print", false, null);}catch(e){print();}' + ((!opts["leaveOpen"] && opts["printMode"].toLowerCase() == 'popup') ? 'close();' : '') + '}</script>');
         html.push('</body></html>');
 
         return html.join('');
     };
-})(window, jQuery);
+}(window, jQuery));
